56c6a96c1847977b620ec4b2b0a055995be074b5
Dorian JAMINAIS <dorian@jaminais.fr>
2013-01-04 18:28:26 +0100

    parsing argument
    handling argument for source directory and output directory

--- dobble.py
+++ dobble.py
@@ -5,27 +5,28 @@
 	the pictures contained in the folder.
 """
 
-#import random
+import argparse
 
-def list_symbols():
+
+def list_symbols(source):
 	"""
 	List all symbols to use for the game
 	"""
-	pass
+	return 1
 
 
 def generate_cards(symbols):
 	"""
 	Generate the list of cards.
 	"""
-	pass
+	return 1
 
 
 def draw_cards(symbols):
 	"""
 	Render the game as png files.
 	"""
-	pass
+	return 1
 
 
 def parse_arguments():
@@ -33,11 +34,22 @@ def parse_arguments():
 	Parse argument to determine where to pick pictures from and 
 	where to output the cards.
 	"""
-	pass
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-s', '--source', dest='source', 
+	                   help='folder containing the photos')
+	parser.add_argument('-o', '--output', dest='output', 
+	                   help='destination folder for the cards.')
+
+	args = parser.parse_args()
+
+	assert(args.source)
+	assert(args.output)
+
+	return args.source, args.output
 
 
 if __name__ == '__main__':
 	source, target = parse_arguments()
 	symbols = list_symbols(source)
 	cards = generate_cards(symbols)
-	draw_cards()
+	draw_cards(cards)
